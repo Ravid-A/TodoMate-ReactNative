69de22fdf6c3b6307a1724fbb4ef02263736db37
ravid <ravid2903@gmail.com>
2024-07-01 14:27:43 +0300

    Changed the functionality of Add Task

--- screens/AddTaskScreen.js
+++ screens/AddTaskScreen.js
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
-import { View, StyleSheet, ScrollView } from "react-native";
-import { TextInput, Button, Appbar } from "react-native-paper";
+import { View, StyleSheet, ScrollView, FlatList, Text } from "react-native";
+import { TextInput, Button, IconButton } from "react-native-paper";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
+import Toast from "react-native-toast-message";
 
 import AppHeader from "../components/AppHeader";
 
 const AddTaskScreen = ({ navigation }) => {
   const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [dueDate, setDueDate] = useState("");
+  const [dueDate, setDueDate] = useState();
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  const [tasks, setTasks] = useState([{ id: 0, text: "", completed: false }]);
+  const [nextId, setNextId] = useState(1);
 
   const showDatePicker = () => {
     setDatePickerVisibility(true);
@@ -20,20 +22,72 @@ const AddTaskScreen = ({ navigation }) => {
   };
 
   const handleConfirm = (date) => {
-    setDueDate(date.toDateString());
+    setDueDate(date);
     hideDatePicker();
   };
 
   const handleSave = () => {
+    if (!title) {
+      Toast.show({
+        type: "error",
+        text1: "Error",
+        text2: "Please enter a title",
+      });
+      return;
+    }
+    if (tasks.some((task) => !task.text)) {
+      Toast.show({
+        type: "error",
+        text1: "Error",
+        text2: "Please fill in all tasks",
+      });
+      return;
+    }
+    if (!dueDate) {
+      Toast.show({
+        type: "error",
+        text1: "Error",
+        text2: "Please select a due date",
+      });
+      return;
+    }
     // Implement save functionality
-    console.log("Saving task:", { title, description, dueDate });
+    // dueDate as a unix timestamp
+    console.log("Saving task:", { title, tasks, dueDate: dueDate.getTime() });
+    return;
+
     navigation.goBack();
+  };
+
+  const removeTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+
+    if (!tasks.length) {
+      setTasks([{ id: 0, text: "", completed: false }]);
+      setNextId(1);
+      return;
+    }
+
+    setTasks((tasks) => {
+      tasks = tasks.map((task, index) => {
+        task.id = index;
+        return task;
+      });
+
+      setNextId(tasks.length);
+      return tasks;
+    });
   };
 
   // Get tomorrow's date
   const tomorrow = new Date();
   tomorrow.setDate(tomorrow.getDate() + 1);
   tomorrow.setHours(0, 0, 0, 0);
+
+  // Max date is 1 year from now
+  const maxDate = new Date();
+  maxDate.setFullYear(maxDate.getFullYear() + 1);
+  maxDate.setHours(0, 0, 0, 0);
 
   return (
     <View style={styles.container}>
@@ -43,7 +97,6 @@ const AddTaskScreen = ({ navigation }) => {
         addGoBack={true}
         hasPreviousScreen={true}
       />
-
       <ScrollView style={styles.content}>
         <TextInput
           label="Task Title"
@@ -52,39 +105,65 @@ const AddTaskScreen = ({ navigation }) => {
           mode="outlined"
           style={styles.input}
         />
-
-        <TextInput
-          label="Task Description"
-          value={description}
-          onChangeText={setDescription}
-          mode="outlined"
-          multiline
-          numberOfLines={10}
-          style={styles.input}
+        <Text>Tasks</Text>
+        <FlatList
+          data={tasks}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <View style={styles.taskContainer}>
+              <TextInput
+                label="Task"
+                value={item.text}
+                onChangeText={(text) =>
+                  setTasks(
+                    tasks.map((task) =>
+                      task.id === item.id ? { ...task, text } : task
+                    )
+                  )
+                }
+                mode="outlined"
+                style={styles.taskInput}
+              />
+              <IconButton
+                icon="close"
+                size={20}
+                onPress={() => removeTask(item.id)}
+                style={styles.removeButton}
+              />
+            </View>
+          )}
         />
-
+        <Button
+          mode="contained"
+          onPress={() => {
+            setTasks([...tasks, { id: nextId, text: "", completed: false }]);
+            setNextId(nextId + 1);
+          }}
+          style={styles.button}
+        >
+          Add a new task
+        </Button>
         <TextInput
           label="Due Date"
-          value={dueDate}
+          value={dueDate?.toDateString()}
           mode="outlined"
           editable={false}
           style={styles.input}
         />
-
         <Button mode="contained" onPress={showDatePicker} style={styles.button}>
           Select Due Date
         </Button>
-
         <Button mode="contained" onPress={handleSave} style={styles.button}>
           Save Task
         </Button>
-
         <DateTimePickerModal
           isVisible={isDatePickerVisible}
+          date={dueDate ? dueDate : tomorrow}
           mode="date"
           onConfirm={handleConfirm}
           onCancel={hideDatePicker}
           minimumDate={tomorrow}
+          maximumDate={maxDate}
         />
       </ScrollView>
     </View>
@@ -103,6 +182,18 @@ const styles = StyleSheet.create({
   },
   button: {
     marginBottom: 16,
+  },
+  taskContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 8,
+  },
+  taskInput: {
+    flex: 1,
+    marginRight: 8,
+  },
+  removeButton: {
+    marginLeft: 8,
   },
 });
 
